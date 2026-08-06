12fc46592cd8b3c902238a78b1005401e3045d43
RITURAJ KUMAR <riturajrajce2005@gmail.com>
2026-08-06 12:07:54 +0530

    Adder more queries

--- sql/04_Basic_queries.sql
+++ sql/04_Basic_queries.sql
@@ -15,7 +15,9 @@ where state in ('jharkhand','bihar');
 select * from products;
 select product_name , stock
 from products;
+-- Average product price
 select avg(price) from products;
+-- Price greater than the their average prices
 select product_name,price
 from products
 having price > (
@@ -35,5 +37,29 @@ where price =(
     select  max(price) 
     from products
     );
-
+-- top 3 cheapest product
+select product_name , price
+from products
+order by price asc
+limit 3;
+-- count total stock
+select sum(stock) as total_stock
+from products;
+-- Product which stock greater than 20
+select product_id , product_name,stock
+from products
+where stock > 20;
+-- Order with status delivered
+select * from orders
+where order_status = 'delivered';
+-- Order status is diff. from delivered
+select * from orders
+where order_status != 'delivered';
+-- Payments made using 'UPI'
+select * from payments
+where payment_method = 'upi';
+-- Customers whose last name start with S
+select firs_name,last_name
+from customers
+where last_name is like to %s;
 
